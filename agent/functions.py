1d118a7634f3e10d7b4b11adcd8e6b01a57704c0
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-26 23:39:09 +0700

    Create functions.py

---
+++ agent/functions.py
@@ -0,0 +1,18 @@
+functions = [
+    {
+        "name": "get_time",
+        "description": "Get current server time",
+        "parameters": {
+            "type": "object",
+            "properties": {}
+        }
+    },
+    {
+        "name": "ping",
+        "description": "Ping test",
+        "parameters": {
+            "type": "object",
+            "properties": {}
+        }
+    }
+]
