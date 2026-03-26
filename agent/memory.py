ddb37564970fd03c4b1956084b6e41cd744ba5db
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-26 23:37:30 +0700

    Update memory.py

--- agent/memory.py
+++ agent/memory.py
@@ -1,1 +1,16 @@
+memory = {}
 
+
+def add(user, text):
+
+    if user not in memory:
+        memory[user] = []
+
+    memory[user].append(text)
+
+    memory[user] = memory[user][-10:]
+
+
+def get(user):
+
+    return memory.get(user, [])
